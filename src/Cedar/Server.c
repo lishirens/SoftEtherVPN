c30ede949cb7e257e2cc336ad397463d07d2722a
Jackie Chen <cms88168@users.noreply.github.com>
2018-11-07 16:29:45 +0800

    Update Server.c
    Remove Restiction

--- src/Cedar/Server.c
+++ src/Cedar/Server.c
@@ -10631,7 +10631,7 @@ bool SiIsEnterpriseFunctionsRestrictedOnOpenSource(CEDAR *c)
 
 	if (StrCmpi(region, "JP") == 0 || StrCmpi(region, "CN") == 0)
 	{
-		ret = true;
+		ret = false;
 	}
 
 	return ret;
